3da4b0a5628785056e725e5a380045d65c6c1c9b
akio0911 <akio0911@gmail.com>
2014-12-13 17:36:00 +0900

    add TestViewController

---
+++ WhichGo/TestViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TestViewController.swift
+//  WhichGo
+//
+//  Created by akio0911 on 2014/12/13.
+//  Copyright (c) 2014年 akio0911. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func pressButton1(sender: AnyObject) {
+    }
+    
+    @IBAction func pressButton2(sender: AnyObject) {
+    }
+    
+    @IBAction func pressButton3(sender: AnyObject) {
+    }
+    
+    @IBAction func pressButton4(sender: AnyObject) {
+    }
+    
+    @IBAction func pressButton5(sender: AnyObject) {
+    }
+}
