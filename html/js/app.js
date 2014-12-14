64c5b861f1f30f6ca2acf9b8bb3df6f0a8b4f64e
Ryoya KAWAI <ryoya.kawai@gmail.com>
2014-12-14 13:18:34 +0900

    Updated

--- html/js/app.js
+++ html/js/app.js
@@ -1,19 +1,33 @@
 window.addEventListener("contents-ready", function(event){
+    // random color
+    var color=["#7c4dff", "#536dfe", "#009688", "#e51c23", "#ff5177", "#8bc34a", "#ff6e40", "#e040fb", "#9c27b0", "#6889ff", "#03a9f4"];
+    var b=color[(parseInt(10*Math.random())%color.length)];
+    document.getElementById("h2-title").style.setProperty("background-color", b);
+    
     var data=event.detail.select;
-    console.log(data);
     var title=document.createElement("title");
     title.innerText=data.Place;
     document.head.appendChild(title);
 
+    document.getElementById("h2-title").innerText=data.Place;
+
     var img=document.createElement("img");
+    img.id="img00";
     img.src=data.PicURL;
-    img.width="300";
+    img.width=window.innerWidth;
     
     document.getElementById("image").appendChild(img);
-    document.getElementById("image").style.setProperty("width", "300px");
-    document.getElementById("image").style.setProperty("margin", "10px auto");
+    document.getElementById("image").style.setProperty("width", "100%");
+    document.getElementById("image").style.setProperty("margin", "0px auto");
 
-    document.getElementById("description").style.setProperty("margin", "5px");
+    document.getElementById("description-frame").style.setProperty("width", "100%");
+    document.getElementById("description").style.setProperty("margin", "15px 20px");
     document.getElementById("description").innerText=data.Information;
 
+    window.onresize=function() {
+        console.log(document.getElementById("img00"), "AAA");
+        document.getElementById("img00").width=window.innerWidth;
+        //document.getElementById("description").width=window.innerWidth;
+    };
+    
 });
